af01b3c530712ef64212e0821189dc742ad330d4
Matt Oliver <protogonoi@gmail.com>
2020-04-26 01:28:26 +1000

    project: Update for 1.0.3 merge.

--- SMP/fribidi-config.h
+++ SMP/fribidi-config.h
@@ -7,10 +7,10 @@
 #define FRIBIDI_NAME "GNU FriBidi"
 #define FRIBIDI_BUGREPORT "http://bugs.freedesktop.org/enter_bug.cgi?product=fribidi"
 
-#define FRIBIDI_VERSION "v1.0.2"
+#define FRIBIDI_VERSION "v1.0.3"
 #define FRIBIDI_MAJOR_VERSION v1
 #define FRIBIDI_MINOR_VERSION 0
-#define FRIBIDI_MICRO_VERSION 2
+#define FRIBIDI_MICRO_VERSION 3
 #define FRIBIDI_INTERFACE_VERSION 4
 #define FRIBIDI_INTERFACE_VERSION_STRING "4"
 
@@ -18,6 +18,8 @@
 #define FRIBIDI_SIZEOF_INT 4
 
 /* Define if fribidi was built with MSVC */
+#ifndef FRIBIDI_ENTRY
 #define FRIBIDI_ENTRY
+#endif
 
 #endif /* FRIBIDI_CONFIG_H */
